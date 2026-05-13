14dad77af603608060c24ede6ffab702e44c0ba1
ishara6madusanka <jayathu-cs22004@stu.kln.ac.lk>
2026-05-13 14:16:46 +0530

    Created the App Caterory model and table

---
+++ Tracking/App_Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TaskForge.Tracking
+{
+    public class App_Category
+    {
+        [Key]
+        public string Category { get; set; } = "Neutral";
+
+        public string AppName { get; set; }
+
+    }
+}
